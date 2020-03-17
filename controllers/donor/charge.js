e50d5a0299a67afd4751ffea653b6e1694a095f2
Omar Elbarmawi <oelbarmawi@gmail.com>
2020-03-17 15:04:23 -0700

    comment

--- controllers/donor/charge.js
+++ controllers/donor/charge.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 module.exports.set = function(app) {
     app.post('/donor/charge', (req, res) => {
         // TOOD: Loop through transactions and obtain daily total
+        // Should we go through our database of transactions for the day and charge based on that?
         let donorId = req.get('donorId');
         let amount = req.get('amount');
         let date = moment().format("YYYY-MM-DD HH:mm:ss");
