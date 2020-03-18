db6914f3df24ac88ef037921cbd586a616b57fe3
Omar Elbarmawi <oelbarmawi@gmail.com>
2020-03-18 01:41:29 -0700

    Added foundation for swagger docs

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require('express');
 const mysql = require('mysql');
 const controllers = require('./controllers');
 const db = require('./database');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
 controllers.set(app);
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: "Sadaqa API",
+        description: "Backend API for Sadaqa",
+        contact: {
+          name: "Omar Elbarmawi"
+        },
+        servers: ["http://localhost:5000"]
+      }
+    },
+    apis: ['app.js', './controllers/*.js', './controllers/*/*.js', ]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => {
--- controllers/sample.js
+++ controllers/sample.js
@@ -1,4 +1,23 @@
 module.exports.set = function(app) {
+    /**
+     * @swagger
+     * /sample:
+     *    get:
+     *      tags:
+     *        - Sample
+     *      description: Sample route
+     *      parameters:
+     *        - name: donorId
+     *          in: header
+     *          description: Donor identification number
+     *          required: true
+     *          schema:
+     *            type: string
+     *            format: string
+     *      responses:
+     *        '200':
+     *          description: A successful response
+     */
     app.get('/sample', (req, res) => {
         res.send('Sample is working.');
     });
